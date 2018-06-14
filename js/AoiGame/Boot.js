4ab9508188b4c2dc415aaad0f087befa65f41e83
238 <f0rg4tip@gmail.com>
2018-06-14 18:45:46 +0900

    add en touch to aoi

--- js/AoiGame/Boot.js
+++ js/AoiGame/Boot.js
@@ -24,6 +24,7 @@ BasicGame.Boot.prototype={
 			MAIN_STROKE_COLOR: BasicGame.MAIN_STROKE_COLOR,
 			WHITE_COLOR: BasicGame.WHITE_COLOR,
 			TOUCH_OR_CLICK: (this.game.device.touch)?'タッチ':'クリック',
+			EN_TOUCH_OR_CLICK: (this.game.device.touch)?'TOUCH':'CLICK',
 			BG_COUNT:12,
 		});
 		this.M.defineGlobal({
--- js/AoiGame/Preloader.js
+++ js/AoiGame/Preloader.js
@@ -113,7 +113,8 @@ BasicGame.Preloader.prototype={
 		this.game.device.desktop&&(document.body.style.cursor='pointer');
 		this.M.SE.setSounds(this.sounds);
 		this.M.H.setSPBrowserColor(BasicGame.MAIN_COLOR);
-		this.M.S.genText(this.world.centerX, this.world.centerY*1.5,this.M.getConst('TOUCH_OR_CLICK')+'してスタート',{fontSize:30});
+		this.M.S.genText(this.world.centerX, this.world.centerY*1.7,
+			this.M.getConst('TOUCH_OR_CLICK')+'してスタート\n'+this.M.getConst('EN_TOUCH_OR_CLICK')+' TO PLAY',{fontSize:25});
 		this.stage.disableVisibilityChange=!1;
 		this.game.input.onDown.add(this.start,this);
 	},
