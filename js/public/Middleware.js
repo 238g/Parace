1ca5c12fd0d4ed20c4464ed387ee69e49c488cdd
238 <f0rg4tip@gmail.com>
2018-06-14 17:33:00 +0900

    Update Middleware.js

--- js/public/Middleware.js
+++ js/public/Middleware.js
@@ -56,6 +56,7 @@ Middleware.prototype = {
 		}
 		Scene.scale.parentIsWindow = true;
 		Scene.scale.refresh();
+return;
 		this.GameObj.TOUCH_OR_CLICK=(Scene.game.device.touch)?'タッチ':'クリック';
 		this.GameObj.EN_TOUCH_OR_CLICK=(Scene.game.device.touch)?'TOUCH':'CLICK';
 	},
