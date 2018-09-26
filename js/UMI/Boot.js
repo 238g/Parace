8de8840d73af4edb72795ce665acfb5fa0db4016
238g <f0rg4tip@gmail.com>
2018-09-26 12:05:26 +0900

    test ga console index and oda and fix umi lang

--- js/UMI/Boot.js
+++ js/UMI/Boot.js
@@ -14,7 +14,8 @@ init:function(){this.M.BootInit(!1)},
 preload:function(){this.load.atlasJSONHash('loading','images/loading/loading.png','images/loading/loading.json')},
 create:function(){
 	this.M.dGlb({
-		curLang:(this.M.H.getQuery('lang')=='en')?'en':'jp',
+		curLang:'jp',
+		// curLang:(this.M.H.getQuery('lang')=='en')?'en':'jp',
 		playCount:0,
 		endTut:!1,
 		curStg:1,
