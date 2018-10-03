d41fd415eb81d9303824f5a697f8ed699d421fb4
238g <f0rg4tip@gmail.com>
2018-10-03 18:54:22 +0900

    fix honeystrap

--- js/HoneyStrap/Play.js
+++ js/HoneyStrap/Play.js
@@ -14,9 +14,15 @@ BasicGame.Play.prototype={
 		this.LevelInfo=this.M.gGlb('LevelInfo');
 		this.curLevelInfo=this.LevelInfo[this.curLevel];
 		// Val
-
+		this.earthRad=250;
+		this.playerRad=50;
+		this.worldG=.8;
+		this.playerSpeed=1;
+		this.closeToSpike=10;
+		this.farFromSpike=25;
 
 		// Obj
+		this.Earth=this.Player=this.Spikes=
 		null;
 		this.Tween={};
 	},
@@ -27,10 +33,39 @@ BasicGame.Play.prototype={
 		// this.playBgm();
 		this.genContents();
 		// this.M.gGlb('endTut')?this.genStart():this.genTut();
+		this.start();//TODO del
 		this.tes();
 	},
 	update:function(){
+		if(this.isPlaying){
+			if(this.Player.jumps>0){
+				this.Player.jumpOffset+=this.Player.jumpForce;
+				this.Player.jumpForce-=this.worldG;
+				if(this.Player.jumpOffset<0){
+					this.Player.jumpOffset=0;
+					this.Player.jumps=0;
+					this.Player.jumpForce=0;
+				}
+			}
+			this.Player.curAngle=Phaser.Math.wrapAngle(this.Player.curAngle+this.playerSpeed);
+			var rad=Phaser.Math.degToRad(this.Player.curAngle);
+			var distanceFromCenter=(this.earthRad+this.playerRad)/2+this.Player.jumpOffset;
+			this.Player.x=this.Earth.x+distanceFromCenter*Math.cos(rad);
+			this.Player.y=this.Earth.y+distanceFromCenter*Math.sin(rad);
+			var revolutions=this.earthRad/this.playerRad+1;
+			this.Player.angle=this.Player.curAngle*revolutions;
 
+			this.Spikes.forEach(function(spike){
+			// this.Spikes.children.iterate(function(spike){
+				var angleDiff=this.getAngleDifference(spike.angle,this.Player.curAngle);
+				if(!spike.approaching&&angleDiff<this.closeToSpike){
+					spike.approaching=!0;
+				}
+				if(spike.approaching&&angleDiff>this.farFromSpike){
+					this.placeSpike(spike,(spike.quadrant+3)%4);
+				}
+			},this);
+		}
 	},
 	start:function(){this.isPlaying=this.inputEnabled=!0},
 	end:function(){this.isPlaying=this.inputEnabled=!1},
@@ -45,11 +80,48 @@ BasicGame.Play.prototype={
 	},
 	////////////////////////////////////// PlayContents
 	genContents:function(){
+		this.Earth=this.M.S.genBmpCclSp(this.world.centerX,this.world.centerY,this.earthRad,'#fff');
+		this.Earth.anchor.setTo(.5);
+		this.genPlayer();
 
-
-		// TODO @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-		
+		this.Spikes=this.add.group();
+		for(var i=0;i<6;i++){
+			var spike=this.M.S.genBmpSqrSp(0,0,50,50,'#0000ff');
+			spike.anchor.setTo(0,.5);
+			this.Spikes.add(spike);
+			this.placeSpike(spike,Math.floor(i/2));
+		}
+	},
+	genPlayer:function(){
+		this.Player=this.M.S.genBmpCclSp(this.world.centerX,this.world.height-this.earthRad/2-this.playerRad,this.playerRad,'#00ff00');
+		this.Player.anchor.setTo(.5);
+		this.Player.curAngle=-90;
+		this.Player.jumpOffset=0;
+		this.Player.jumps=0;
+		this.Player.jumpForce=0;
+		this.input.onDown.add(this.jump,this);
+		this.Player.addChild(this.M.S.genBmpCclSp(0,0,10,'#ff0000'));//TODO del
+	},
+	jump:function(){
+		if(this.Player.jumps<2){
+			this.Player.jumps++;
+			this.Player.jumpForce=this.Player.jumps==1?12:8;
+		}
+	},
+	placeSpike:function(spike,quadrant){
+		var rndAngle=Phaser.Math.between(quadrant*90,(quadrant+1)*90);
+		rndAngle=Phaser.Math.wrapAngle(rndAngle);
+		var rndAngleRad=Phaser.Math.degToRad(rndAngle);
+		spike.x=this.Earth.x+(this.earthRad*.5-4)*Math.cos(rndAngleRad);
+		spike.y=this.Earth.y+(this.earthRad*.5-4)*Math.sin(rndAngleRad);
+		spike.quadrant=quadrant;
+		spike.angle=rndAngle;
+		spike.approaching=!1;
+	},
+	getAngleDifference:function(a1,a2){
+		var angleDifference=a1-a2;
+		angleDifference+=(angleDifference>180)?-360:(angleDifference<-180)?360:0;
+		return Math.abs(angleDifference);
 	},
 	genHUD:function(){
 		this.HUD=this.add.group();
