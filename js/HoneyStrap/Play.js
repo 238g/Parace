82c63127d0be05070ecb1ada0537963acb6630b3
238 <f0rg4tip@gmail.com>
2018-10-05 00:30:39 +0900

    fix honeystrap

--- js/HoneyStrap/Play.js
+++ js/HoneyStrap/Play.js
@@ -15,14 +15,20 @@ BasicGame.Play.prototype={
 		this.curLevelInfo=this.LevelInfo[this.curLevel];
 		// Val
 		this.earthRad=250;
-		this.playerRad=50;
+		this.playerRad=25;
 		this.worldG=.8;
-		this.playerSpeed=1;
-		this.closeToSpike=10;
+		this.playerSpeed=1.5;
+		this.closeToSpike=5;
 		this.farFromSpike=25;
+		this.revolutions=this.earthRad/this.playerRad+1;
+		this.spikeW=5;
+		this.spikeH=30;
+		this.jumpForce=[12,9,6];
 
 		// Obj
 		this.Earth=this.Player=this.Spikes=
+		// this.Graphics=
+		// this.FollowEff=this.DmgEff=
 		null;
 		this.Tween={};
 	},
@@ -33,6 +39,7 @@ BasicGame.Play.prototype={
 		// this.playBgm();
 		this.genContents();
 		// this.M.gGlb('endTut')?this.genStart():this.genTut();
+		// this.time.events.add(500,this.start,this);//TODO del
 		this.start();//TODO del
 		this.tes();
 	},
@@ -48,21 +55,48 @@ BasicGame.Play.prototype={
 				}
 			}
 			this.Player.curAngle=Phaser.Math.wrapAngle(this.Player.curAngle+this.playerSpeed);
+
+			// this.maskImage.angle = this.player.currentAngle + 90;
+
 			var rad=Phaser.Math.degToRad(this.Player.curAngle);
 			var distanceFromCenter=(this.earthRad+this.playerRad)/2+this.Player.jumpOffset;
 			this.Player.x=this.Earth.x+distanceFromCenter*Math.cos(rad);
 			this.Player.y=this.Earth.y+distanceFromCenter*Math.sin(rad);
-			var revolutions=this.earthRad/this.playerRad+1;
-			this.Player.angle=this.Player.curAngle*revolutions;
+			// var revolutions=this.earthRad/this.playerRad+1;
+			this.Player.angle=this.Player.curAngle*this.revolutions;
+
+			// this.Graphics.clear();
 
 			this.Spikes.forEach(function(spike){
-			// this.Spikes.children.iterate(function(spike){
 				var angleDiff=this.getAngleDifference(spike.angle,this.Player.curAngle);
 				if(!spike.approaching&&angleDiff<this.closeToSpike){
 					spike.approaching=!0;
 				}
-				if(spike.approaching&&angleDiff>this.farFromSpike){
-					this.placeSpike(spike,(spike.quadrant+3)%4);
+
+				if(spike.approaching){
+					/*
+					this.Graphics.beginFill(0xff0000);
+					this.Graphics.lineStyle(4,0xff0000,1);
+					this.Graphics.moveTo(spike.topA.x,spike.topA.y);
+					this.Graphics.lineTo(spike.base1.x,spike.base1.y);
+					this.Graphics.endFill();
+					this.Graphics.beginFill(0xff0000);
+					this.Graphics.moveTo(spike.topA.x,spike.topA.y);
+					this.Graphics.lineTo(spike.base2.x,spike.base2.y);
+					this.Graphics.endFill();
+					*/
+					if(this.distToSegmentSquared(
+						new Phaser.Point(this.Player.x,this.Player.y),
+						this.playerRad,spike.topA,spike.base1)
+						||this.distToSegmentSquared(
+							new Phaser.Point(this.Player.x,this.Player.y),
+							this.playerRad,spike.topA,spike.base2)){
+						this.hitSpike();
+					}
+
+					if(angleDiff>this.farFromSpike){
+						this.placeSpike(spike,(spike.quadrant+3)%4);
+					}
 				}
 			},this);
 		}
@@ -82,46 +116,102 @@ BasicGame.Play.prototype={
 	genContents:function(){
 		this.Earth=this.M.S.genBmpCclSp(this.world.centerX,this.world.centerY,this.earthRad,'#fff');
 		this.Earth.anchor.setTo(.5);
+
+		this.genEff();
+
 		this.genPlayer();
 
+		// this.Graphics=this.add.graphics();
 		this.Spikes=this.add.group();
-		for(var i=0;i<6;i++){
-			var spike=this.M.S.genBmpSqrSp(0,0,50,50,'#0000ff');
+		for(var i=0;i<9;i++){
+			var spike=this.M.S.genBmpSqrSp(0,0,this.spikeH,this.spikeW,'#ffffff');
 			spike.anchor.setTo(0,.5);
 			this.Spikes.add(spike);
-			this.placeSpike(spike,Math.floor(i/2));
+			this.placeSpike(spike,Math.floor(i/3));
 		}
+
+		// this.maskImage = this.add.sprite(game.config.width / 2, game.config.height / 2, "mask");
+	},
+	genEff:function(){
+		// TODO
+		// this.FollowEff=this.add.emitter();
+
+		/*
+		this.DmgEff=this.add.emitter();
+		this.DmgEff.minParticleScale=0;
+		this.DmgEff.maxParticleScale=1;
+		this.DmgEff.minParticleAlpha=0;
+		this.DmgEff.maxParticleAlpha=1;
+		this.DmgEff.frequency=100;
+		this.DmgEff.lifespan=1E3;
+		*/
 	},
 	genPlayer:function(){
-		this.Player=this.M.S.genBmpCclSp(this.world.centerX,this.world.height-this.earthRad/2-this.playerRad,this.playerRad,'#00ff00');
+		this.Player=this.M.S.genBmpCclSp(0,0,this.playerRad,'#00ff00');
+		// this.Player=this.M.S.genBmpCclSp(this.world.centerX,this.world.height-this.earthRad*.5-this.playerRad,this.playerRad,'#00ff00');
 		this.Player.anchor.setTo(.5);
-		this.Player.curAngle=-90;
+		this.Player.curAngle=-80;
 		this.Player.jumpOffset=0;
 		this.Player.jumps=0;
 		this.Player.jumpForce=0;
+
+		var rad=Phaser.Math.degToRad(this.Player.curAngle);
+		var distanceFromCenter=(this.earthRad+this.playerRad)/2+this.Player.jumpOffset;
+		this.Player.x=this.Earth.x+distanceFromCenter*Math.cos(rad);
+		this.Player.y=this.Earth.y+distanceFromCenter*Math.sin(rad);
+
 		this.input.onDown.add(this.jump,this);
 		this.Player.addChild(this.M.S.genBmpCclSp(0,0,10,'#ff0000'));//TODO del
 	},
 	jump:function(){
-		if(this.Player.jumps<2){
+		if(this.Player.jumps<this.jumpForce.length){
 			this.Player.jumps++;
-			this.Player.jumpForce=this.Player.jumps==1?12:8;
+			this.Player.jumpForce=this.jumpForce[this.Player.jumps-1];
 		}
 	},
 	placeSpike:function(spike,quadrant){
 		var rndAngle=Phaser.Math.between(quadrant*90,(quadrant+1)*90);
 		rndAngle=Phaser.Math.wrapAngle(rndAngle);
 		var rndAngleRad=Phaser.Math.degToRad(rndAngle);
-		spike.x=this.Earth.x+(this.earthRad*.5-4)*Math.cos(rndAngleRad);
-		spike.y=this.Earth.y+(this.earthRad*.5-4)*Math.sin(rndAngleRad);
+		var spikeX=this.Earth.x+(this.earthRad*.5-this.rnd.between(4,25))*Math.cos(rndAngleRad);
+		spike.x=spikeX;
+		var spikeY=this.Earth.y+(this.earthRad*.5-this.rnd.between(4,25))*Math.sin(rndAngleRad);
+		spike.y=spikeY;
 		spike.quadrant=quadrant;
 		spike.angle=rndAngle;
 		spike.approaching=!1;
+		spike.topA=new Phaser.Point(spikeX+this.spikeH*Math.cos(rndAngleRad),spikeY+this.spikeH*Math.sin(rndAngleRad));
+		spike.base1=new Phaser.Point(spikeX+this.spikeW/2*Math.cos(rndAngleRad+Math.PI/2),spikeY+this.spikeW/2*Math.sin(rndAngleRad+Math.PI/2));
+		spike.base2=new Phaser.Point(spikeX+this.spikeW/2*Math.cos(rndAngleRad-Math.PI/2),spikeY+this.spikeW/2*Math.sin(rndAngleRad-Math.PI/2));
 	},
 	getAngleDifference:function(a1,a2){
 		var angleDifference=a1-a2;
 		angleDifference+=(angleDifference>180)?-360:(angleDifference<-180)?360:0;
 		return Math.abs(angleDifference);
+	},
+    getDistance:function(p1,p2){
+        return (p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y);
+    },
+	distToSegmentSquared:function(circleCenter,circleRad,segmentStart,segmentEnd){
+		var l2=this.getDistance(segmentStart,segmentEnd);
+		var t=((circleCenter.x-segmentStart.x)*(segmentEnd.x-segmentStart.x)+(circleCenter.y-segmentStart.y)*(segmentEnd.y-segmentStart.y))/l2;
+		t=Math.max(0,Math.min(1,t));
+		var tX=segmentStart.x+t*(segmentEnd.x-segmentStart.x);
+		var tY=segmentStart.y+t*(segmentEnd.y-segmentStart.y);
+		var tPoint={x:tX,y:tY};
+		return this.getDistance(circleCenter,tPoint)<circleRad*circleRad;
+	},
+	hitSpike:function(){
+		if(this.isPlaying){
+			this.camera.shake(.01,800,!0,Phaser.Camera.SHAKE_BOTH);
+			// TODO eff
+			console.log('END');
+			this.end();
+			this.time.events.add(1E3,function(){
+				this.M.NextScene('Play');
+			},this);
+			this.Player.visible=!1;
+		}
 	},
 	genHUD:function(){
 		this.HUD=this.add.group();
